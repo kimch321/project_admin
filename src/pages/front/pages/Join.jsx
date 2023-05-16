2715082c3c63373fae12be38341dbb717526c0c8
KIM CHUNG IL <cnddlf12@naver.com>
2023-05-17 03:02:40 +0900

    버튼 디자인 결정

--- src/pages/front/pages/Join.jsx
+++ src/pages/front/pages/Join.jsx
@@ -5,13 +5,12 @@ import "./Join.css"
 
 export default function Join () {
     return (
-        <div className="registerMain">
+        <div className="registerMain mt-5">
             <Row>
-                <Col className={"offset-lg-4 col-lg-4 pt-4 joinContainer"}>
-                    <h2 className={"text-center mb-5"}>회원가입</h2>
-                    <hr />
-                    <div id="joinForm" className="ps-3">
-                        <Row className={"mb-3"}>
+                <Col className={"offset-xl-4 col-xl-4 joinContainer"}>
+                    <h3 className={"text-center registerHeader"} style={{marginBottom:"60px"}}>회원가입</h3>
+                    <div id="joinForm" className="ps-3 border-top border-2 border-dark">
+                        <Row className={"mb-4 mt-3"}>
                             <Col className={"col-3 align-self-center"}>
                                 <p style={{margin:"0"}}>이메일 <span className={"text-danger"}>*</span></p>
                             </Col>
@@ -19,10 +18,10 @@ export default function Join () {
                                 <input type="text" style={{width:"100%",height:"40px"}} />
                             </Col>
                             <Col className={"col-3"}>
-                               <Button>중복확인</Button>
+                               <button className="joinButton">중복확인</button>
                             </Col>
                         </Row>
-                        <Row className={"mb-3"}>
+                        <Row className={"mb-4"}>
                             <Col className={"col-3 align-self-center"}>
                                 <p style={{margin:"0"}}>닉네임 <span className={"text-danger"}>*</span></p>
                             </Col>
@@ -30,23 +29,23 @@ export default function Join () {
                                 <input type="text" style={{width:"100%",height:"40px"}} />
                             </Col>
                         </Row>
-                        <Row className={"mb-3"}>
+                        <Row className={"mb-4"}>
                             <Col className={"col-3 align-self-center"}>
                                 <p style={{margin:"0"}}>비밀번호 <span className={"text-danger"}>*</span></p>
                             </Col>
                             <Col className={"col-6"}>
-                                <input type="text" style={{width:"100%",height:"40px"}} />
+                                <input type="password" style={{width:"100%",height:"40px"}} />
                             </Col>
                         </Row>
-                        <Row className={"mb-3"}>
+                        <Row className={"mb-4"}>
                             <Col className={"col-3 align-self-center"}>
                                 <p style={{margin:"0"}}>비밀번호 확인 <span className={"text-danger"}>*</span></p>
                             </Col>
                             <Col className={"col-6"}>
-                                <input type="text" style={{width:"100%",height:"40px"}} />
+                                <input type="password" style={{width:"100%",height:"40px"}} />
                             </Col>
                         </Row>
-                        <Row className={"mb-3"}>
+                        <Row className={"mb-4"}>
                             <Col className={"col-3 align-self-center"}>
                                 <p style={{margin:"0"}}>이름 <span className={"text-danger"}>*</span></p>
                             </Col>
@@ -54,7 +53,7 @@ export default function Join () {
                                 <input type="text" style={{width:"100%",height:"40px"}} />
                             </Col>
                         </Row>
-                        <Row className={"mb-3"}>
+                        <Row className={"mb-4"}>
                             <Col className={"col-3 align-self-center"}>
                                 <p style={{margin:"0"}}>휴대폰 번호 <span className={"text-danger"}>*</span></p>
                             </Col>
@@ -62,15 +61,15 @@ export default function Join () {
                                 <input type="text" style={{width:"100%",height:"40px"}} />
                             </Col>
                         </Row>
-                        <Row className={"mb-3"}>
+                        <Row className={"mb-4"}>
                             <Col className={"col-3 align-self-center"}>
-                                <p style={{margin:"0"}}>생년 월일 <span className={"text-danger"}>*</span></p>
+                                <p style={{margin:"0"}}>구분 <span className={"text-danger"}>*</span></p>
                             </Col>
                             <Col className={"col-6"}>
                                 <input type="text" style={{width:"100%",height:"40px"}} />
                             </Col>
                         </Row>
-                        <Row className={"mb-3"}>
+                        <Row className={"mb-4"}>
                             <Col className={"col-3 align-self-center"}>
                                 <p style={{margin:"0"}}>성별 <span className={"text-danger"}>*</span></p>
                             </Col>
@@ -80,15 +79,15 @@ export default function Join () {
                         </Row>
                         <Row className={"mb-5"}>
                             <Col className={"col-3 align-self-center"}>
-                                <p style={{margin:"0"}}>구분 <span className={"text-danger"}>*</span></p>
+                                <p style={{margin:"0"}}>생년 월일 <span className={"text-danger"}>*</span></p>
                             </Col>
                             <Col className={"col-6"}>
                                 <input type="text" style={{width:"100%",height:"40px"}} />
                             </Col>
                         </Row>
                         <Row style={{marginBottom:"100px"}}>
-                            <Col className={"offset-4 col-4"}>
-                                <Button style={{width:"100%"}}>가입하기</Button>
+                            <Col className={"offset-3 col-6 d-flex justify-content-center"}>
+                                <button className="joinSubmitButton">가입하기</button>
                             </Col>
                         </Row>
                     </div>
