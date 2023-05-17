b00b768973ffc2c2a99196075ddbacd272e669ec
KIM CHUNG IL <cnddlf12@naver.com>
2023-05-18 00:35:53 +0900

    케러셀 이미지 변경

--- src/pages/front/pages/ClassMain.jsx
+++ src/pages/front/pages/ClassMain.jsx
@@ -26,7 +26,7 @@ export default function ClassMain() {
                     <div className="d-flex justify-content-center">
                         <img
                             className="d-block rounded"
-                            src="https://images.unsplash.com/photo-1639048759250-47646c3f3c0d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
+                            src="https://images.unsplash.com/photo-1661956601349-f61c959a8fd4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1471&q=80"
                             alt="Second slide"
                             width="1200px"
                             height="400px"
@@ -41,7 +41,7 @@ export default function ClassMain() {
                     <div className="d-flex justify-content-center">
                         <img
                             className="d-block rounded"
-                            src="https://plus.unsplash.com/premium_photo-1663050712656-f240e3cdd7d3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
+                            src="https://images.unsplash.com/photo-1684166220037-d94a15745f9b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
                             alt="Third slide"
                             width="1200px"
                             height="400px"
