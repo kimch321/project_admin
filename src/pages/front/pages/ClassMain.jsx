c40dcd3718c79cf6136eeb8a9099a9c4563d7a10
KIM CHUNG IL <cnddlf12@naver.com>
2023-05-23 02:02:40 +0900

    메인 nav, 카드 반응형 고려

--- src/pages/front/pages/ClassMain.jsx
+++ src/pages/front/pages/ClassMain.jsx
@@ -11,7 +11,7 @@ import others from "../img/nav/007-more.png";
 import bannerSale1 from "../img/banner/webBanner01.png"
 import bannerLemon1 from "../img/banner/webBanner02.png"
 import pupucook from "../img/banner/webBanner03.png"
-import {Carousel, Col, Row} from "react-bootstrap";
+import {Carousel, Col, Container, Row} from "react-bootstrap";
 import Card from 'react-bootstrap/Card';
 import {HiLocationMarker} from "react-icons/hi";
 import {VscFilterFilled} from "react-icons/vsc";
@@ -22,74 +22,78 @@ import {ImStarEmpty, ImStarFull, ImStarHalf} from "react-icons/im";
 export default function ClassMain() {
     return(
         <>
-            <Carousel className=" mt-4 mb-5 ">
-                <Carousel.Item>
-                    <div className="d-flex justify-content-center">
-                        <img
-                            className="d-block rounded"
-                            src={bannerSale1}
-                            alt="First slide"
-                            width="912"
-                            height="380"
-                        />
-                    </div>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <div className="d-flex justify-content-center">
-                        <img
-                            className="d-block rounded"
-                            src={bannerLemon1}
-                            alt="Second slide"
-                            width="912"
-                            height="380"
-                        />
-                    </div>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <div className="d-flex justify-content-center">
-                        <img
-                            className="d-block rounded"
-                            src={pupucook}
-                            alt="Third slide"
-                            width="912"
-                            height="380"
-                        />
-                    </div>
-                </Carousel.Item>
-            </Carousel>
+                <Carousel className=" mt-4 mb-5 ">
+                    <Carousel.Item>
+                        <div className="d-flex justify-content-center">
+                            <img
+                                className="d-block rounded"
+                                src={bannerSale1}
+                                alt="First slide"
+                                width="50%"
+                                height="380"
+                            />
+                        </div>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <div className="d-flex justify-content-center">
+                            <img
+                                className="d-block rounded"
+                                src={bannerLemon1}
+                                alt="Second slide"
+                                width="50%"
+                                height="380"
+                            />
+                        </div>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <div className="d-flex justify-content-center">
+                            <img
+                                className="d-block rounded"
+                                src={pupucook}
+                                alt="Third slide"
+                                width="50%"
+                                height="380"
+                            />
+                        </div>
+                    </Carousel.Item>
+                </Carousel>
 
-            <Row>
-                <Col className="offset-xl-3 col-xl-6">
-                    <div className="mainNavContainer">
-                        <ul className={"d-flex justify-content-between text-center flex-wrap list-unstyled mb-5"}>
-                            <li className="px-3 pb-1 pt-2" style={{"--clr":"#fd7e14"}}><img src={all} alt="all" /><p className={"mt-1"}>전체</p></li>
-                            <li className="px-3 pb-1 pt-2" style={{"--clr":"#6c757d"}}><img src={fitness} alt="fitness" /><p className={"mt-1"}>피트니스</p></li>
-                            <li className="px-3 pb-1 pt-2" style={{"--clr":"#faad14"}}><img src={cook} alt="cook" /><p className={"mt-1"}>요리</p></li>
-                            <li className="px-3 pb-1 pt-2" style={{"--clr":"#f27781"}}><img src={craft} alt="craft" /><p className={"mt-1"}>공예</p></li>
-                            <li className="px-3 pb-1 pt-2" style={{"--clr":"#6f42c1"}}><img src={music} alt="music" /><p className={"mt-1"}>음악</p></li>
-                            <li className="px-3 pb-1 pt-2" style={{"--clr":"#52c41a"}}><img src={art} alt="art" /><p className={"mt-1"}>미술</p></li>
-                            <li className="px-3 pb-1 pt-2" style={{"--clr":"#e83e8c"}}><img src={activity} alt="activity" /><p className={"mt-1"}>액티비티</p></li>
-                            <li className="px-3 pb-1 pt-2" style={{"--clr":"#3B4CA8"}}><img src={others} alt="others" /><p className={"mt-1"}>기타</p></li>
-                        </ul>
-                    </div>
-                </Col>
-            </Row>
-
-            <Row>
-                <Col className="offset-xl-3 col-xl-6  mb-2" style={{cursor:"pointer"}}>
-                    <span className="border border-1 border-dark rounded-pill px-3 py-1 border-opacity-25" style={{fontSize:"16px"}}>
-                       <VscFilterFilled className="mb-1" /> 필터
-                    </span>
-                </Col>
-            </Row>
-
-
-            <div className="mainCardContainer">
+            <Container>
                 <Row>
-                    <Col className="offset-xl-3 col-xl-6 mb-5">
+                    <Col xl={2} />
+                    <Col xl={8}>
+                        <div className="mainNavContainer">
+                            <ul className={"d-flex justify-content-between text-center flex-wrap list-unstyled mb-5"}>
+                                <li className="px-3 pb-1 pt-2" style={{"--clr":"#fd7e14"}}><img src={all} alt="all" /><p className={"mt-1"}>전체</p></li>
+                                <li className="px-3 pb-1 pt-2" style={{"--clr":"#6c757d"}}><img src={fitness} alt="fitness" /><p className={"mt-1"}>피트니스</p></li>
+                                <li className="px-3 pb-1 pt-2" style={{"--clr":"#faad14"}}><img src={cook} alt="cook" /><p className={"mt-1"}>요리</p></li>
+                                <li className="px-3 pb-1 pt-2" style={{"--clr":"#f27781"}}><img src={craft} alt="craft" /><p className={"mt-1"}>공예</p></li>
+                                <li className="px-3 pb-1 pt-2" style={{"--clr":"#6f42c1"}}><img src={music} alt="music" /><p className={"mt-1"}>음악</p></li>
+                                <li className="px-3 pb-1 pt-2" style={{"--clr":"#52c41a"}}><img src={art} alt="art" /><p className={"mt-1"}>미술</p></li>
+                                <li className="px-3 pb-1 pt-2" style={{"--clr":"#e83e8c"}}><img src={activity} alt="activity" /><p className={"mt-1"}>액티비티</p></li>
+                                <li className="px-3 pb-1 pt-2" style={{"--clr":"#3B4CA8"}}><img src={others} alt="others" /><p className={"mt-1"}>기타</p></li>
+                            </ul>
+                        </div>
+                    </Col>
+                    <Col xl={2} />
+                </Row>
+
+                <Row>
+                    <Col className="offset-xl-2 col-xl-8  mb-2" style={{cursor:"pointer"}}>
+                        <span className="border border-1 border-dark rounded-pill px-3 py-1 border-opacity-25" style={{fontSize:"16px"}}>
+                           <VscFilterFilled className="mb-1" /> 필터
+                        </span>
+                    </Col>
+                </Row>
+            </Container>
+
+
+            <Container className="mainCardContainer">
+                <Row>
+                    <Col className="offset-xl-2 col-xl-8 mb-5">
                         <Row>
-                            <Col className="col-4 mt-4">
-                                <Card border="light" style={{ width: '18rem' }}>
+                            <Col className="col-xl-4 mt-4">
+                                <Card border="light" className="mx-auto" style={{ width: '18rem' }}>
                                     <Card.Img variant="top" src="https://s3.ap-northeast-2.amazonaws.com/umclassuploadboardimg/3d6446b750d54cff8e2f8eba1c946358.jpg" width="100%" height="218px" />
                                     <Card.Body>
                                         <div className="d-flex py-1">
@@ -103,8 +107,8 @@ export default function ClassMain() {
                                     </Card.Body>
                                 </Card>
                             </Col>
-                            <Col className="col-4  mt-4">
-                                <Card border="light" style={{ width: '18rem' }}>
+                            <Col className="col-xl-4  mt-4">
+                                <Card border="light" className="mx-auto" style={{ width: '18rem' }}>
                                     <Card.Img variant="top" src="https://s3.ap-northeast-2.amazonaws.com/umclassuploadboardimg/107245ea89a24b5484bef665c9a3b947.jpg" width="100%" height="218px" />
                                     <Card.Body>
                                         <div className="d-flex py-1">
@@ -118,8 +122,8 @@ export default function ClassMain() {
                                     </Card.Body>
                                 </Card>
                             </Col>
-                            <Col className="col-4 mt-4">
-                                <Card border="light" style={{ width: '18rem' }}>
+                            <Col className="col-xl-4 mt-4">
+                                <Card border="light" className="mx-auto"style={{ width: '18rem' }}>
                                     <Card.Img variant="top" src="https://s3.ap-northeast-2.amazonaws.com/umclassuploadboardimg/9aaa99bb62c44b38adcf441b5256b782.jpg" width="100%" height="218px" />
                                     <Card.Body>
                                         <div className="d-flex py-1">
@@ -133,8 +137,8 @@ export default function ClassMain() {
                                     </Card.Body>
                                 </Card>
                             </Col>
-                            <Col className="col-4 mt-4">
-                                <Card border="light" style={{ width: '18rem' }}>
+                            <Col className="col-xl-4 mt-4">
+                                <Card border="light" className="mx-auto"style={{ width: '18rem' }}>
                                     <Card.Img variant="top" src="https://s3.ap-northeast-2.amazonaws.com/umclassuploadboardimg/ede7841e7fd242259f201dc7dd73a91d.jpg" width="100%" height="218px" />
                                     <Card.Body>
                                         <div className="d-flex py-1">
@@ -148,8 +152,8 @@ export default function ClassMain() {
                                     </Card.Body>
                                 </Card>
                             </Col>
-                            <Col className="col-4 mt-4">
-                                <Card border="light" style={{ width: '18rem' }}>
+                            <Col className="col-xl-4 mt-4">
+                                <Card border="light" className="mx-auto"style={{ width: '18rem' }}>
                                     <Card.Img variant="top" src="https://s3.ap-northeast-2.amazonaws.com/umclassuploadboardimg/c573170218414d0ca49314b80559c780.jpg" width="100%" height="218px" />
                                     <Card.Body>
                                         <div className="d-flex py-1">
@@ -163,8 +167,8 @@ export default function ClassMain() {
                                     </Card.Body>
                                 </Card>
                             </Col>
-                            <Col className="col-4 mt-4">
-                                <Card border="light" style={{ width: '18rem' }}>
+                            <Col className="col-xl-4 mt-4">
+                                <Card border="light" className="mx-auto"style={{ width: '18rem' }}>
                                     <Card.Img variant="top" src="https://s3.ap-northeast-2.amazonaws.com/umclassuploadboardimg/4eb9426e3d1f433eb0c22242d97edfc2.jpg" width="100%" height="218px" />
                                     <Card.Body>
                                         <div className="d-flex py-1">
@@ -178,8 +182,8 @@ export default function ClassMain() {
                                     </Card.Body>
                                 </Card>
                             </Col>
-                            <Col className="col-4 mt-4">
-                                <Card border="light" style={{ width: '18rem' }}>
+                            <Col className="col-xl-4 mt-4">
+                                <Card border="light" className="mx-auto"style={{ width: '18rem' }}>
                                     <Card.Img variant="top" src="https://s3.ap-northeast-2.amazonaws.com/umclassuploadboardimg/57b1ad459a8f40e4bc0e0e2914977fb1.JPG" width="100%" height="218px" />
                                     <Card.Body>
                                         <div className="d-flex py-1">
@@ -193,8 +197,8 @@ export default function ClassMain() {
                                     </Card.Body>
                                 </Card>
                             </Col>
-                            <Col className="col-4 mt-4">
-                                <Card border="light" style={{ width: '18rem' }}>
+                            <Col className="col-xl-4 mt-4">
+                                <Card border="light" className="mx-auto"style={{ width: '18rem' }}>
                                     <Card.Img variant="top" src="https://s3.ap-northeast-2.amazonaws.com/umclassuploadboardimg/5988c7de06a14b45bd8354cc285f092c.jpg" width="100%" height="218px" />
                                     <Card.Body>
                                         <div className="d-flex py-1">
@@ -208,8 +212,8 @@ export default function ClassMain() {
                                     </Card.Body>
                                 </Card>
                             </Col>
-                            <Col className="col-4 mt-4">
-                                <Card border="light" style={{ width: '18rem' }}>
+                            <Col className="col-xl-4 mt-4">
+                                <Card border="light" className="mx-auto"style={{ width: '18rem' }}>
                                     <Card.Img variant="top" src="https://s3.ap-northeast-2.amazonaws.com/umclassuploadboardimg/18a922ad06e74794a5bce4fa68d30264.JPG" width="100%" height="218px" />
                                     <Card.Body>
                                         <div className="d-flex py-1">
@@ -223,8 +227,8 @@ export default function ClassMain() {
                                     </Card.Body>
                                 </Card>
                             </Col>
-                            <Col className="col-4 mt-4">
-                                <Card border="light" style={{ width: '18rem' }}>
+                            <Col className="col-xl-4 mt-4">
+                                <Card border="light" className="mx-auto"style={{ width: '18rem' }}>
                                     <Card.Img variant="top" src="https://s3.ap-northeast-2.amazonaws.com/umclassuploadboardimg/c16d3620cc6548109303d7033f62ab24.jpg" width="100%" height="218px" />
                                     <Card.Body>
                                         <div className="d-flex py-1">
@@ -238,8 +242,8 @@ export default function ClassMain() {
                                     </Card.Body>
                                 </Card>
                             </Col>
-                            <Col className="col-4 mt-4">
-                                <Card border="light" style={{ width: '18rem' }}>
+                            <Col className="col-xl-4 mt-4">
+                                <Card border="light" className="mx-auto"style={{ width: '18rem' }}>
                                     <Card.Img variant="top" src="https://s3.ap-northeast-2.amazonaws.com/umclassuploadboardimg/f01c9f5cbdb641b1adc91c6c02c07300.jpg" width="100%" height="218px" />
                                     <Card.Body>
                                         <div className="d-flex py-1">
@@ -253,8 +257,8 @@ export default function ClassMain() {
                                     </Card.Body>
                                 </Card>
                             </Col>
-                            <Col className="col-4 mt-4">
-                                <Card border="light" style={{ width: '18rem' }}>
+                            <Col className="col-xl-4 mt-4">
+                                <Card border="light" className="mx-auto"style={{ width: '18rem' }}>
                                     <Card.Img variant="top" src="https://s3.ap-northeast-2.amazonaws.com/umclassuploadboardimg/74f88105855744a19c1e308d48d696f7.jpg" width="100%" height="218px" />
                                     <Card.Body>
                                         <div className="d-flex py-1">
@@ -272,7 +276,7 @@ export default function ClassMain() {
                         </Row>
                     </Col>
                 </Row>
-            </div>
+            </Container>
 
 
         </>
